712976ac67594d447aa580320bcff06f9cf1f422
acnesiac <acnesiac@hotmail.com>
2020-09-10 01:46:06 -0500

    changes in docker compose

--- src/main/java/com/ust/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ust.model;
-
-public enum Role {
-	USER, PUBLISHER
-}
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('administrador1', '$2a$10$RC1.YFSL8o2vCIg6kB3fjuBB639BdjY6AD8rIw9oyWJLws/1Q6Lku', TRUE, 'ADMIN');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('administrador2', '$2a$10$MUahUza86ErCxtsgpmMBDeR5VtoGHioRdl03/jQmkM/sk6L.Eg28e', TRUE, 'ADMIN');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('user1', '$2a$10$WcgRF8VQ8DKt4h4Hz9pWv.6MXnIRmcPr0j9jqsseprsBwTD4w8WSm', TRUE, 'USER');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('user2', '$2a$10$Q5bxyPXhHXFc1fRUMCRWR.GbgsXx9aGZdoEoEAz2JFEfckdyUKfOi', TRUE, 'USER');
-
