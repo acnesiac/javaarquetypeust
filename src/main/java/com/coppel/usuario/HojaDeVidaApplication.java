1489dc41b57b17036d7227764fb82a858f10329b
acnesiac <acnesiac@hotmail.com>
2020-09-21 15:51:32 -0500

    maven 2 MSSQL dependency

--- src/main/java/com/coppel/usuario/HojaDeVidaApplication.java
+++ src/main/java/com/coppel/usuario/HojaDeVidaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HojaDeVidaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HojaDeVidaApplication.class, args);
     }
---
+++ src/main/java/com/coppel/usuario/SQLDatabaseConnection.java
@@ -0,0 +1,33 @@
+package com.coppel.usuario;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLDatabaseConnection {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;"
+                        + "database=master;"
+                        + "user=sa;"
+                        + "password=Pa$$word;"
+                        + "encrypt=false;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try  {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            Connection connection = DriverManager.getConnection(connectionUrl);
+
+            // Code here.
+            connection.close();
+        }
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
